a33734878b7f49de8b5ef097761fbca3af3347c6
rodBarrs <rodrigosbarros51@gmail.com>
2023-01-14 18:36:52 -0300

    melhorando a coesão classe Funcionário

---
+++ rh/src/br/com/alura/rh/service/Reajuste.java
@@ -0,0 +1,13 @@
+ public class ReajusteService {
+
+    public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento){
+        BigDecima salarioAtual = funcionario.getSalario();
+        BigDecimal percentualReajuste = aumento.divide(salario, RoundingMode.HALF_UP);
+        if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
+            throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
+        }
+        BigDecimal salarioReajustado = salarioAtual.add(aumento);
+        funcionario.atualizarSalario();
+    }
+
+ }
